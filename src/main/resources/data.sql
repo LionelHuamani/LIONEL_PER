6f6ebe277b0613ccdd3edb6001f9d5f44d68a587
LionelHuamani <lionel.huamani.levano@vallegrande.edu.pe>
2025-04-12 21:12:21 +0000

    feat: add new product table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,12 @@
 -- Crear el registro si es que no se duplican datos unicos o se insertan datos nulos
 INSERT INTO customer (dni, first_name, last_name, state)
 VALUES ('12345678', 'Juan', 'Sanchez', 'A');
+
+INSERT INTO product (name, description, unit_price, state) 
+VALUES 
+    ('Martillo de 16 onzas', 'Martillo de acero con mango de madera', 12.99, 'A'),
+    ('Destornillador Phillips', 'Destornillador de punta Phillips con mango ergonómico', 4.99, 'A'),
+    ('Taladro inalámbrico', 'Taladro de 12 voltios con batería de litio', 79.99, 'A'),
+    ('Sierra circular', 'Sierra circular de 7.25 pulgadas con hoja de metal', 119.99, 'A'),
+    ('Clavos de acero', 'Clavos de acero galvanizado de 2 pulgadas', 9.99, 'A'),
+    ('Tornillos para madera', 'Tornillos para madera de 2 pulgadas, cabeza plana', 7.99, 'A
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,3 +6,15 @@ CREATE TABLE customer (
     last_name VARCHAR(50) NOT NULL,
     state CHAR(1) NOT NULL
 );
+
+CREATE TABLE product (
+    id INT IDENTITY(1,1) PRIMARY KEY,   
+    name VARCHAR(60) NOT NULL,                  
+    description VARCHAR(200) NOT NULL,              
+    unit_price DECIMAL(4,2) NOT NULL,                 
+    state CHAR(1) NOT NULL                 
+);
+
+SELECT * FROM product;
+
+DROP TABLE product;
