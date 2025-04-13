c88c1283f355f58c50a381c838c95ba7f5014152
LionelHuamani <lionel.huamani.levano@vallegrande.edu.pe>
2025-04-13 04:36:44 +0000

    feat: add new product table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,9 +8,9 @@ VALUES
 
 INSERT INTO product (name, description, unit_price, state) 
 VALUES 
-    ('Martillo de 16 onzas', 'Martillo de acero con mango de madera', 12.99, 'A'),
-    ('Destornillador Phillips', 'Destornillador de punta Phillips con mango ergonómico', 4.99, 'A'),
-    ('Taladro inalámbrico', 'Taladro de 12 voltios con batería de litio', 79.99, 'A'),
-    ('Sierra circular', 'Sierra circular de 7.25 pulgadas con hoja de metal', 119.99, 'A'),
-    ('Clavos de acero', 'Clavos de acero galvanizado de 2 pulgadas', 9.99, 'A'),
-    ('Tornillos para madera', 'Tornillos para madera de 2 pulgadas, cabeza plana', 7.99, 'A
+    ('Martillo', 'Martillo de acero con mango de madera', 12, 'A'),
+    ('Destornillador', 'Destornillador de punta Phillips con mango ergonómico', 49, 'A'),
+    ('Taladro', 'Taladro de 12 voltios con batería de litio', 79, 'A'),
+    ('Sierra', 'Sierra circular de 7.25 pulgadas con hoja de metal', 19, 'A'),
+    ('Clavos', 'Clavos de acero galvanizado de 2 pulgadas', 99, 'A'),
+    ('Tornillos', 'Tornillos para madera de 2 pulgadas, cabeza plana', 7.99, 'A');
