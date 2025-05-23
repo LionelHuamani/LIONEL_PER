71ab295f59673aa68eaa05200ff61f9063386f2d
LionelHuamani <lionel.huamani.levano@vallegrande.edu.pe>
2025-05-23 16:32:35 +0000

    fix: Bug fixes in master tables

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
--- Active: 1746857006820@@127.0.0.1@1433@model
+-- Active: 1748016413094@@127.0.0.1@1433@model
 -- insertamos datos en customer
 INSERT INTO customer (name, address, type_document, number_document, phone, email)
 VALUES 
